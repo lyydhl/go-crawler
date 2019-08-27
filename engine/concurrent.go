33000e287ab009e8de0acc0ed035d7fe9f1c5331
ZhanCheng <1185230223@qq.com>
2019-08-27 16:04:32 +0800

    爬取珍爱网用户信息第三个版本(并发队列)

--- engine/concurrent.go
+++ engine/concurrent.go
@@ -1,6 +1,8 @@
 package engine
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type ConcurrentEngine struct {
 	Scheduler   Scheduler
@@ -10,16 +12,63 @@ type ConcurrentEngine struct {
 type Scheduler interface {
 	Submit(Request)
 	ConfigureMasterWorkerChan(chan Request)
+	WorkerReady(chan Request)
+	Run()
 }
 
+/* 第二版 */
+// func (e *ConcurrentEngine) Run(seed ...Request) {
+
+// 	in := make(chan Request)
+// 	out := make(chan ParserResult)
+// 	e.Scheduler.ConfigureMasterWorkerChan(in)
+
+// 	for i := 0; i < e.WorkerCount; i++ {
+// 		createWorker(in, out)
+// 	}
+
+// 	for _, r := range seed {
+// 		e.Scheduler.Submit(r)
+// 	}
+
+// 	itemCount := 0
+// 	for {
+// 		result := <-out
+// 		for _, item := range result.Items {
+// 			fmt.Printf("Got item #%d %v\n", itemCount, item)
+// 			itemCount++
+// 		}
+
+// 		for _, request := range result.Requests {
+// 			e.Scheduler.Submit(request)
+// 		}
+// 	}
+
+// }
+
+// func createWorker(in chan Request, out chan ParserResult) {
+// 	go func() {
+// 		for {
+// 			request := <-in
+// 			result, err := worker(request)
+// 			if err != nil {
+// 				continue
+// 			}
+// 			out <- result
+// 		}
+// 	}()
+// }
+
+/* 第二版 */
+
+/* 第三版 */
 func (e *ConcurrentEngine) Run(seed ...Request) {
 
-	in := make(chan Request)
 	out := make(chan ParserResult)
-	e.Scheduler.ConfigureMasterWorkerChan(in)
+	e.Scheduler.Run()
 
 	for i := 0; i < e.WorkerCount; i++ {
-		createWorker(in, out)
+		createWorker(out, e.Scheduler)
 	}
 
 	for _, r := range seed {
@@ -41,9 +90,11 @@ func (e *ConcurrentEngine) Run(seed ...Request) {
 
 }
 
-func createWorker(in chan Request, out chan ParserResult) {
+func createWorker(out chan ParserResult, s Scheduler) {
+	in := make(chan Request)
 	go func() {
 		for {
+			s.WorkerReady(in)
 			request := <-in
 			result, err := worker(request)
 			if err != nil {
@@ -53,3 +104,5 @@ func createWorker(in chan Request, out chan ParserResult) {
 		}
 	}()
 }
+
+/* 第三版 */
--- main.go
+++ main.go
@@ -7,13 +7,26 @@ import (
 )
 
 func main() {
+	//第一个版本
 	// engine.SimpleEngine{}.Run(engine.Request{
 	// 	Url:       "http://www.zhenai.com/zhenghun",
 	// 	ParserFun: parser.ParserCityList,
 	// })
 
+	//第二个版本
+	//限制10个城市 /zhenai/parser/citylist
+	// e := engine.ConcurrentEngine{
+	// 	Scheduler:   &scheduler.SimpleScheduler{},
+	// 	WorkerCount: 100,
+	// }
+	// e.Run(engine.Request{
+	// 	Url:       "http://www.zhenai.com/zhenghun",
+	// 	ParserFun: parser.ParserCityList,
+	// })
+
+	//第三个版本
 	e := engine.ConcurrentEngine{
-		Scheduler:   &scheduler.SimpleScheduler{},
+		Scheduler:   &scheduler.QueuedScheduler{},
 		WorkerCount: 100,
 	}
 	e.Run(engine.Request{
